7361c0437763c462e538bdb3526c95908df9b6d9
Keval <kevalravani06@gmail.com>
2024-12-03 23:40:15 +0530

    Add files via upload

---
+++ creating arrays.py
@@ -0,0 +1,24 @@
+import numpy as np
+arr = np.array([2,4,6])
+print(arr)
+def multiply():
+    arr = np.array([2,4,6])
+    arr = arr * 2
+    print(arr)
+def div():
+    arr = np.array([2,4,6])
+    arr = arr/2
+    print(arr)
+def sub():
+    arr = np.array([2,4,6])
+    arr = arr - 2
+    print(arr)
+def add():
+    arr = np.array([2,4,6])
+    arr = arr + 10
+    print(arr)
+
+multiply()
+div()
+sub()
+add()

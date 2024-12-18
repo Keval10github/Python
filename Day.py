a0d6fc3c6ab85a5815d15e15b76deeb1f3d63ce1
Keval <kevalravani06@gmail.com>
2024-12-18 19:29:23 +0530

    Add files via upload

---
+++ Day.py
@@ -0,0 +1,40 @@
+no = int(input("Enter value of no = "))
+
+if no==1:
+    print("Mon")
+if no==2:
+    print("Tue")
+if no==3:
+    print("Wed")
+if no==4:
+    print("Thu")
+if no==5:
+    print("Fri")
+if no==6:
+    print("Sat")
+if no==7:
+    print("Sun")
+if no>7:
+    print("Invalid")
+if no<1:
+    print("Invalid")
+
+
+
+if no==1:
+    print("Mon")
+elif no==2:
+    print("Tue")
+elif no==3:
+    print("Wed")
+elif no==4:
+    print("Thu")
+elif no==5:
+    print("Fri")
+elif no==6:
+    print("Sat")
+elif no==7:
+    print("Sun")
+else:
+    print("Invalid")
+

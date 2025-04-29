a832c09cf0b016c3a6f26a8800a5b5e365e92b6d
Keval <kevalravani06@gmail.com>
2025-04-29 11:53:42 +0530

    Add files via upload

---
+++ Greater or smaller(2 number).py
@@ -0,0 +1,8 @@
+no1=int(input(""))
+no2=int(input(""))
+if no1 > no2 :
+    print("number 1 greater than 2")
+if no2 > no1 :
+    print("number 2 greater than 1")
+if no1==no2 :
+    print("both are equal")
